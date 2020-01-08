eff0914e2ebee7f024364a8d553719dd36fe2669
acrylic-style <me@acrylicstyle.xyz>
2020-01-09 01:43:50 +0900

    bug fix

--- maintenance/invalidateCache.js
+++ maintenance/invalidateCache.js
@@ -1,5 +1,7 @@
 const cache = require('../src/cache')
 const arg = process.argv[2]
 if (!arg) return console.error('please specify cache id!')
-cache.invalidateCache(arg)
-cache.save().then(() => console.log(`done, cache '${arg}' has been invalidated.`))
+cache.loadFile().then(() => {
+  cache.invalidateCache(arg)
+  cache.save().then(() => console.log(`done, cache '${arg}' has been invalidated.`))
+})
