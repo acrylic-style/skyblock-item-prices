101c9f36fd6f29e1875a171db1e253fea0a92017
acrylic-style <me@acrylicstyle.xyz>
2020-11-11 15:03:57 +0900

    add messages

--- maintenance/invalidateCache.js
+++ maintenance/invalidateCache.js
@@ -1,8 +1,10 @@
-console.log("starting...")
 const cache = require('../src/cache')
 const arg = process.argv[2]
 if (!arg) return console.error('please specify cache id!')
+console.log('loading cache')
 cache.loadFile().then(() => {
+  console.log('invalidating cache')
   cache.invalidateCache(arg)
+  console.log('writing cache to disk')
   cache.save().then(() => console.log(`done, cache '${arg}' has been invalidated.`))
 })
